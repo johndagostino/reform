94e62099464ce6c5666ab86633239d1e50b25837
Nick Sutterer <apotonick@gmail.com>
2015-08-18 13:06:24 +1000

    why did that get deleted?

---
+++ lib/reform/validation.rb
@@ -0,0 +1,34 @@
+# Adds ::validates and friends, and #valid? to the object.
+# This is completely form-independent.
+module Reform::Validation
+  module ClassMethods
+    def validation_groups
+      @groups ||= Groups.new(validation_group_class) # TODO: inheritable_attr with Inheritable::Hash
+    end
+
+    # DSL.
+    def validation(name, options={}, &block)
+      group = validation_groups.add(name, options)
+
+      group.instance_exec(&block)
+    end
+
+    def validates(name, options)
+      validation(:default, inherit: true) { validates name, options }
+    end
+
+    def validate(*args, &block)
+      validation(:default, inherit: true) { validate *args, &block }
+    end
+  end
+
+  def self.included(includer)
+    includer.extend(ClassMethods)
+  end
+
+  def valid?
+    Groups::Result.new(self.class.validation_groups).(@fields, errors, self)
+  end
+end
+
+require "reform/validation/groups"
